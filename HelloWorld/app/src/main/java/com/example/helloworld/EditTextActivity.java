8de8f6645889cd263647cd091b2a6fe74c345a7a
pontoontan <279062613@qq.com>
2020-03-01 22:20:07 +0800

    init

--- HelloWorld/app/src/main/java/com/example/helloworld/EditTextActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/EditTextActivity.java
@@ -3,12 +3,52 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditTextActivity extends AppCompatActivity {
+
+    private EditText mEtUserName;
+    private Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+
+        mBtnLogin = (Button) findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 登录成功弹出消息
+                Toast.makeText(EditTextActivity.this, "登录成功!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mEtUserName = (EditText) findViewById(R.id.et_1);
+        // 监听输入框内容变化事件
+        mEtUserName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // s: 输入框中的内容
+                Log.d("edittext", s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,36 +12,55 @@ public class MainActivity extends AppCompatActivity {
     // 申明 Button 组件
     private Button mBtnTextView; // 定义名称
     private Button mBtnEditorText; // 定义名称
+    private Button mBtnRadioButton; // 定义名称
+    private Button mBtnCheckBox; // 定义名称
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 引入布局
         setContentView(R.layout.activity_main);
+
         // 通过 id 找到 btn_textview
         mBtnTextView = (Button) findViewById(R.id.btn_textview);
-        // 设置一个点击事件
-        mBtnTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 跳转到 TextView 演示界面
-                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
-                startActivity(intent);
-            }
-        });
-
         mBtnEditorText = (Button) findViewById(R.id.btn_edittext);
-        mBtnEditorText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 跳转到 TextView 演示界面
-                Intent intent = new Intent(MainActivity.this, EditTextActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
+        mBtnRadioButton = (Button) findViewById(R.id.btn_radiobutton);
+        mBtnCheckBox = (Button) findViewById(R.id.btn_checkbox);
+        setLisenners();
     }
+
+    private void setLisenners(){
+        Onclick onClick = new Onclick();
+        mBtnTextView.setOnClickListener(onClick);
+        mBtnEditorText.setOnClickListener(onClick);
+        mBtnRadioButton.setOnClickListener(onClick);
+        mBtnCheckBox.setOnClickListener(onClick);
+    }
+
+    private class Onclick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.btn_textview:
+                    // 跳转到 TextViewActivity
+                    intent = new Intent(MainActivity.this, TextViewActivity.class);
+                    break;
+                case R.id.btn_edittext:
+                    // 跳转到 EditTextActivity
+                    intent = new Intent(MainActivity.this, EditTextActivity.class);
+                    break;
+                case R.id.btn_radiobutton:
+                    // 跳转到 RadioButtonActivity
+                    intent = new Intent(MainActivity.this, RadioButtonActivity.class);
+                    break;
+                case R.id.btn_checkbox:
+                    intent = new Intent(MainActivity.this, CheckBoxActivity.class);
+            }
+            startActivity(intent);
+        }
+    }
+
 }
+
